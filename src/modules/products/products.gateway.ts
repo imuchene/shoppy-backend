42f7acff7862f4f3b1455c4904f639f950e2f641
Isaiah Muchene <imuchene@msn.com>
2025-06-10 17:26:31 +0300

    Disconnected the websocket if a user is not logged in

--- src/modules/products/products.gateway.ts
+++ src/modules/products/products.gateway.ts
@@ -1,19 +1,25 @@
 import {
   OnGatewayConnection,
+  OnGatewayDisconnect,
   WebSocketGateway,
   WebSocketServer,
   WsException,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { AuthService } from '../auth/auth.service';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class ProductsGateway implements OnGatewayConnection {
+export class ProductsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly authService: AuthService) {}
+
+  private logger: Logger = new Logger(ProductsGateway.name);
 
   @WebSocketServer()
   private readonly server: Server;
@@ -23,15 +29,25 @@ export class ProductsGateway implements OnGatewayConnection {
   }
 
   handleConnection(client: Socket) {
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */
+    const token = String(client.handshake.auth.Authentication.value);
+
+    if (!token) {
+      client.disconnect(true);
+      this.logger.error(`Client ${client.id} disconnected. No token provided`);
+      return;
+    }
+
     try {
-      this.authService.verifyToken(
-        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */
-        String(client.handshake.auth.Authentication.value),
-      );
+      this.authService.verifyToken(token);
 
       /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
     } catch (error) {
       throw new WsException('Unauthorized');
     }
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
   }
 }
